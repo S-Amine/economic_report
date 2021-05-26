9a00b714ae95cf604d7bde583c5b58009d3693ac
Saidi Amine <saidi.amine.p@gmail.com>
2021-05-26 20:40:12 +0100

    versio with pdfs downloaded

--- ons_scraping/pdf_downloader.py
+++ ons_scraping/pdf_downloader.py
@@ -7,16 +7,22 @@ import json
 json_file = open('ons_pdf.json')
 data = json.load(json_file)
 # download pdfs
+print("downloading...")
+num = 0
 for link in data :
+    num += 1
     url = link['file_urls'][0]
+    parse = urlparse(url)
+    print(num , 'the file:', str(parse.path), 'is downloading')
     req = urllib.request.Request(url, headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36' })
     gcontext = ssl.SSLContext()  # Only for gangstars
     info = urllib.request.urlopen(req, context=gcontext)
 
     # file download
-    parse = urlparse(url)
-    test = str(parse.path)
-    filename = '.{}'.format(test)
+    path = parse.path
+    name = path.replace(".pdf", "")
+    filename = '.{}'.format(name)
     file = open(filename + ".pdf", 'wb')
     file.write(info.read())
     file.close()
+    print('the download of:', str(parse.path), 'is finished')
